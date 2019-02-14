b19f7d9a5dd5c8759c329e81ae058489cf63a004
jyangjun <jyangjun@outlook.com>
2019-02-14 16:04:13 +0800

    添加test类

---
+++ pinyougoucartservice/src/main/java/com/pinyougou/cart/service/impl/Test.java
@@ -0,0 +1,13 @@
+package com.pinyougou.cart.service.impl;
+
+/**
+ * @author jyangjun
+ * @description
+ * @date 2019/2/14
+ */
+public class Test
+{
+    public static void main(String[] args) {
+        System.out.println("!!!!!");
+    }
+}
